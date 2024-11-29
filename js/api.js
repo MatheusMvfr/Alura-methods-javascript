6ce683aee9f6011eb488d33976c00b8fb688b110
Matheus Vitor Ferreira Ramos <matheusvitorferreiraramos@MacBook-Air-de-Matheus.local>
2024-11-28 21:44:27 -0300

    feat(front): Finalizando o projeto depois de adicionar exemplo usando o Axios.

--- js/api.js
+++ js/api.js
@@ -1,8 +1,10 @@
+const URL_BASE = "http://localhost:3000"
+
 const api = {
     async buscarPensamentos() {
         try {
-            const response = await fetch('http://localhost:3000/pensamentos')
-            return await response.json()
+            const response = await axios.get(`${URL_BASE}/pensamentos`)
+            return await response.data
         } catch {
             alert('Erro ao buscar pensamentos')
             throw error
@@ -11,7 +13,7 @@ const api = {
 
     async salvarPensamento(pensamento) {
         try {
-            const response = await fetch('http://localhost:3000/pensamentos', {
+            const response = await fetch(`${URL_BASE}/pensamentos`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -27,7 +29,7 @@ const api = {
 
     async buscarPensamentoPorId(id) {
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+            const response = await fetch(`${URL_BASE}/pensamentos/${id}`)
             return await response.json()
         } catch {
             alert('Erro ao buscar pensamento')
@@ -36,7 +38,7 @@ const api = {
     }, 
     async editarPensamento(pensamento) {
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+            const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json"
@@ -51,7 +53,7 @@ const api = {
     }, 
     async excluirPensamento(id) {
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${id}`, {
+            const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
                 method: "DELETE"
             })
         } catch {
